29ffa846c6cd19e019d38486190b2f8f5792905f
Max Jacobson <max@hardscrabble.net>
2016-10-07 11:05:23 -0400

    get a better error

--- spec/age_spec.rb
+++ spec/age_spec.rb
@@ -2,24 +2,24 @@ require 'age'
 
 describe Age do
   it "is a simple value object" do
-    expect(Age.new(28)).to be > Age.new(27)
-    expect(Age.new(25)).to be < Age.new(28)
-    expect(Age.new(28)).to be <= Age.new(28)
+    expect(Age.new(28) > Age.new(27)).to be true
+    expect(Age.new(25) < Age.new(28)).to be true
+    expect(Age.new(28) <= Age.new(28)).to be true
     expect(Age.new(28)).to eq Age.new(28)
   end
 
   it "can compare to raw numbers" do
-    expect(Age.new(28)).to be > 27
-    expect(Age.new(25)).to be < 28
-    expect(Age.new(28)).to be <= 28
+    expect(Age.new(28) > 27).to be true
+    expect(Age.new(25) < 28).to be true
+    expect(Age.new(28) <= 28).to be true
     expect(Age.new(28)).to eq 28
   end
 
   it "raw numbers can compare to it" do
     pending "Currently fails :("
-    expect(28).to be > Age.new(27)
-    expect(25).to be < Age.new(28)
-    expect(28).to be <= Age.new(28)
+    expect(28 > Age.new(27)).to be true
+    expect(25 < Age.new(28)).to be true
+    expect(28 <= Age.new(28)).to be true
     expect(28).to eq Age.new(28)
   end
 end
